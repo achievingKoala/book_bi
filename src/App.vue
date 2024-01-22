c9c8e89937fbbb0bbaa20476aef8abacf241ee59
lkw <zgbtlkw@gmail.com>
2024-01-22 03:29:56 +0000

    2

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,98 @@
 <template>
   <div id="app">
     <div>
-      <div class="input-row">
-        巅峰表现 = 
-        <el-input v-for="(input, index) in inputs" :key="index" v-model="inputs[index]" maxlength="10" placeholder="请输入内容" style="width: 200px;"> +
+      <test-view1></test-view1>
+      <el-row>
+        巅峰表现 =
+        <el-input v-for="(input, index) in inputs" :key="index" v-model="inputs[index]" maxlength="10" placeholder="请输入内容"
+          style="width: 200px;"> +
         </el-input>
-      </div>
+        <el-button v-if="isRight" type="success" icon="el-icon-check" circle></el-button>
+      </el-row>
+      <el-row>
+        动机 =
+        <el-input v-for="(input, index) in inputs2" :key="index" v-model="inputs2[index]" maxlength="10" placeholder="请输入内容"
+          style="width: 200px;"> +
+        </el-input>
+        <i  class="el-icon-circle-check"></i>
+        <!-- v-if="isRight2" -->
+      </el-row>
+      <el-row>
+         <span style="color: green;">内在驱动 = </span>
+        <el-input v-for="(input, index) in inputs3" :key="index" v-model="inputs3[index]"  
+          style="width: 200px;color: green;"> +
+        </el-input>
+        <i  class="el-icon-circle-check"></i>
+      </el-row>
     </div>
+
+    <input v-model="textColor" type="text" style="color: green;  font-size: 40px;">
+
+
+    
   </div>
 </template>
 
 <script>
+import TestView1 from './views/TestView1.vue';
 export default {
+  components: {
+    TestView1
+  },
+  created() {
+    var item = {
+      "Chinese":"讨厌艺术的方式有两种……一种是单纯地讨厌它，另一种是理性地喜欢它",
+      "English":"There are two ways of disliking art … One is to dislike it.The other is to like it rationally"
+    }
+    
+    console.log(item);
+  },
   data() {
     return {
+      textColor:'sdfa',
       inputs: ['', '', '', ''],
-      isRight: false
+      inputs2: ['', '', ''],
+      inputs3: ['', '', '','', '', ''],
+      isRight: false,
+      isRight2: false,
+      isRight3: false
     }
   },
   watch: {
     inputs: {
       handler(newInputs) {
-        const rightValues = ['1', '3', '8', '9'];
+        const rightValues = ['动机', '学习', '创造力', '心流'];
         const inputValue = newInputs.join('+');
-        
+
         if (rightValues.join('+') === inputValue) {
           this.isRight = true;
-          alert(this.isRight);
+        }
+      },
+      deep: true
+    },
+    inputs2: {
+      handler(newInputs) {
+        const rightValues = ['驱动力', '目标', '坚毅力'];
+        const inputValue = newInputs.join('+');
+
+        if (rightValues.join('+') === inputValue) {
+          this.isRight2 = true;
+        }
+      },
+      deep: true
+    },
+    inputs3: {
+      handler(newInputs) {
+        const rightValues = ['好奇心', '热情', '意义', '目的', '精通', '掌握感'];
+        const inputValue = newInputs.join('+');
+
+        if (rightValues.join('+') === inputValue) {
+          this.isRight3 = true;
         }
       },
       deep: true
     }
-  }
+  },  
 }
 </script>
 
@@ -39,6 +100,7 @@ export default {
 .input-row {
   display: flex;
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/TestView1.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <!-- Your component's template -->
+        <h1>{{ message }}</h1>
+        <el-row>
+            <input v-for="word in wordList" :key="word" @input="checkInput(word)">
+        </el-row>
+    </div>
+</template>
+  
+<script>
+export default {
+    create() {
+        var item2 = ["There", "are", "two", "ways", "of", "disliking", "art", "One", "is"]
+        this.wordList = item2
+    },
+    data() {
+        return {
+            message: 'Hello, Vue!',
+            wordList:["There", "are", "two", "ways", "of", "disliking", "art", "One", "is"]
+        };
+    }
+};
+</script>
+  
+  
