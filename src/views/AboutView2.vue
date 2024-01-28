20b078db86a3cfdd06bb9fb9b6c3e90256032df0
lkw <zgbtlkw@gmail.com>
2024-01-28 02:39:02 +0000

    1

---
+++ src/views/AboutView2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <medium-editor :text="styledHtmlContent" custom-tag="div" v-model="htmlContent" :options="{ toolbar: { buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote'] } }"></medium-editor>
+
+  </div>
+</template>
+
+<script>
+  import MediumEditor from 'vue2-medium-editor';
+  import 'medium-editor/dist/css/medium-editor.css';
+  
+  export default {
+    data() {
+      return {
+        htmlContent: 'dafdasf <h3>adfa</h3> sadfaaa',
+      };
+    },
+    computed: {
+      styledHtmlContent() {
+        console.log(this.htmlContent)
+        let words = this.htmlContent.split(' ');
+        if (words.length > 1) {
+          words[1] = `<span class="green">${words[1]}</span>`;
+        }
+        return words.join(' ');
+      }
+    },
+    components: {
+      MediumEditor
+    },
+  };
+</script>
+<style scoped>
+.green {
+  color: green;
+}
+</style>
--- src/views/TestView1.vue
+++ src/views/TestView1.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div @keydown="keyMonitor">
         <!-- Your component's template -->
         <!-- <textarea cols= "100" type="text" v-model="textInput" :style="{
             color: textInput === answer ? 'green' : 'blue',
@@ -7,14 +7,17 @@
             height: '200px' 
         }"></textarea> -->
         <!-- <div v-html="htmlContent"></div> -->
-        <!-- <textarea :value="htmlContent"></textarea> -->
-        <!-- <quill-editor v-model="htmlContent"  /> -->
-        <!-- <medium-editor   /> -->
         
-        
-        <el-button ref="myButton">哈哈哈</el-button>
+        {{ htmlContent }}
+        <!-- <medium-editor :text="htmlContent" :content="htmlContent" v-on:edit="logChange"></medium-editor> -->
+        <textarea v-model="htmlContent"></textarea>    
+        <div v-html="htmlContent" contenteditable="true"></div>
+
+    
+        <!-- <el-button ref="myButton">哈哈哈</el-button> -->
 
         <!-- <textarea> <h3>hello</h3> 009 wfw3 <span style="color: green;">9</span></textarea> -->
+        {{ index }}
         <hr>
         <span v-for="(sentence, index) in sentenceList" :key="index">
             <textarea v-model="answerList[index]" :key="index"
@@ -26,8 +29,8 @@
             {{sentenceList[index]}}    
             <hr>
         </span>
-        <el-button>last</el-button>
-        <el-button>next</el-button>
+        <el-button @click="goToLast">last</el-button>
+        <el-button @click="goToNext">next</el-button>
         <div style="border: 1px solid #CBCBCB;height: 200px;width: 400px" contenteditable="true" >
             <span contenteditable="false" >投入：</span><span style="color: red">500</span>
         </div>
@@ -35,12 +38,7 @@
 </template>
   
 <script>
-import { quillEditor } from 'vue-quill-editor';
-import 'quill/dist/quill.core.css';
-import 'quill/dist/quill.snow.css';
-import 'quill/dist/quill.bubble.css';
 
-// import Vue from 'vue';
 import MediumEditor from 'vue2-medium-editor';
 import 'medium-editor/dist/css/medium-editor.css';
 // import 'vue2-medium-editor/dist/vue-medium-editor.css';
@@ -49,47 +47,86 @@ import 'medium-editor/dist/css/medium-editor.css';
 export default {
     components: {
         MediumEditor
-
     },
     created() {
-        console.log('---->', this.$refs.myButton)
-        // this.onContentChange()
         // todo 
         /**
          * color to word
          */
-        var testList1 = ["There are only two kinds of people who are really fascinating", "people who know absolutely everything and people who know absolutely nothing"]
-        var testList1 = ["It is absurd to divide people into good and bad", "People are whether charming or tedious"]
-        var testList1 = ["The public have an insatiable curiosity to know everything", "except what is worth knowing"]
-        var testList1 = ["It is only shallow people who require years to get rid of an emotion", "A man who is master of himself can end a sorrow as easily as he can invent a pleasure"]
-        var testList1 = ["Men who are trying to do something for the world, are always insufferable", "when the world has done something for them, they are charming"]
-        var testList1 = ["I have always been of opinion that hard work is simply the refuge of people who have nothing whatever to do"]
+        var testList8 = ["There are only two kinds of people who are really fascinating", "people who know absolutely everything and people who know absolutely nothing"]
+        var testList7 = ["It is absurd to divide people into good and bad", "People are whether charming or tedious"]
+        var testList6 = ["The public have an insatiable curiosity to know everything", "except what is worth knowing"]
+        var testList5 = ["It is only shallow people who require years to get rid of an emotion", "A man who is master of himself can end a sorrow as easily as he can invent a pleasure"]
+        var testList4 = ["Men who are trying to do something for the world, are always insufferable", "when the world has done something for them, they are charming"]
+        var testList3 = ["I have always been of opinion that hard work is simply the refuge of people who have nothing whatever to do"]
+        var testList2 = ["It is always a silly thing to give advice", "but to give good advice is absolutely fatal","I hope you will never fall into that error", "If you do, you will be sorry for it"]
+        var testList1 = ["Society often forgives the criminal", "it never forgives the dreamer"]
         
-        var item2 = ["There", "are", "two", "ways", "of", "disliking", "art", "One", "is"]
+        this.allDataList.push(testList1);
+        this.allDataList.push(testList2);
+        this.allDataList.push(testList3);
+        this.allDataList.push(testList4);
+        this.allDataList.push(testList5);
+        this.allDataList.push(testList6);
+        this.allDataList.push(testList7);
+        this.allDataList.push(testList8);
+        this.index = 0;
         // this.sentenceList = ['There are two ways of disliking art', "One is to dislike it", "The other is to like it rationally"]
         // this.answerList = ['', '', '']        
-        let myList = Array(testList1.length).fill("");
-        this.answerList = myList;
-        this.sentenceList = testList1;
+        var sList1 = ["Understand ethical wealth creation is possible" , "If you secretly despise wealth","it will elude you"]
+        var sList2 = ["Ignore people playing status games" , "They gain status by attacking people playing wealth creation games"]
+        this.allDataList = []
+        this.allDataList.push(sList1);
+        this.allDataList.push(sList2);
+
     },
     data() {
         return {
-            htmlContent: 'hello',
+            index:-1,
+            htmlContent: '<h3>hello</h3>',
             textInput: '',
             answer: 'There are two ways of disliking art',
-            message: 'Hello, Vue!',
+            message: 'Hello, Vue!',            
+            allDataList: [],
             sentenceList: [],
             answerList: []
         };
     },
     watch: {
-        htmlContent(newValue) {
-            console.log(newValue);
-        },
+        index(indexValue) {
+            this.sentenceList = this.allDataList[indexValue];    
+            let myList = Array(this.sentenceList.length).fill("");
+            this.answerList = myList;
+        }
     },
     methods: {
+        keyMonitor(event) {
+            if (event.key === "[") {
+                this.goToLast();
+            } else if (event.key === "]") {
+                this.goToNext();
+            } 
+            else if (event.key.match(/^[A-Za-z]$/)) {
+                console.log("User input: " + event.key);
+            } else if (event.key === " ") {
+                console.log("hello");
+            } 
+        },
+        logChange(newValue) {
+            console.log(this.htmlContent);
+            // console.log(newValue);
+        },
         goToNext() {
-    
+            this.index ++;
+            if (this.index == this.allDataList.length) {
+                this.index = 0;
+            }
+        },
+        goToLast() {
+            this.index --;
+            if (this.index == -1) {
+                this.index = this.allDataList.length - 1;
+            }
         }
     }
 
